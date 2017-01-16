1ce8f9f414af54bd4e936a09a626912836acb3df
Lavi <lavifb@gmail.com>
2017-01-16 12:13:52 -0800

    reorganized and made setup file for symlinks

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+export DOTHOME=$(pwd)
 
 echo "Setting up symlinks..."
 
 # zsh
+ln -sf $DOTHOME/zsh/zlogin $HOME/.zlogin
+ln -sf $DOTHOME/zsh/zlogout $HOME/.zlogout
+ln -sf $DOTHOME/zsh/zprofile $HOME/.zprofile
+ln -sf $DOTHOME/zsh/zshenv $HOME/.zshenv
+ln -sf $DOTHOME/zsh/zshrc $HOME/.zshrc
 
+ln -sfh $DOTHOME/zsh/zscripts $HOME/.zscripts
 
+# vim
+ln -sf $DOTHOME/vim/init.vim $HOME/.vimrc
+ln -sf $DOTHOME/vim/autoload $HOME/.vim/autoload
+ln -sf $DOTHOME/vim/colors $HOME/.vim/colors
 
+# neovim
+ln -sfh $DOTHOME/vim $HOME/.config/nvim
+
+# git
+ln -sf $DOTHOME/git/gitconfig $HOME/.gitconfig
+
+# tmux
 
 
 echo "Done setting up symlinks."
