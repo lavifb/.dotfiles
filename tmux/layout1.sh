7090f225b676b6e61f270f32a633c71235b941b0
Lavi <lavifb@gmail.com>
2017-01-23 16:30:18 -0800

    added comment about buggy layout file

--- tmux/layout1.sh
+++ tmux/layout1.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# script to make the tmux layout
+# doesn't really work right now though...
 
-tmux splitw -h -p 35
-tmux splitw -v -p 70
-tmux splitw -v -p 50
+tmux new-session -s lay1
+tmux splitw -t 0 -h -p 35
+tmux splitw -t 0 -v -p 70
+tmux splitw -t 0 -v -p 50
 tmux selectp -t 0
-tmux splitw -v -p 35
+tmux splitw -t 0 -v -p 35
