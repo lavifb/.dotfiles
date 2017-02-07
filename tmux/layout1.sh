6465fb998b8ba6ca51d26cd9358533665689bda5
Lavi <lavifb@gmail.com>
2017-02-06 16:23:12 -0800

    something

--- tmux/layout1.sh
+++ tmux/layout1.sh
@@ -2,9 +2,9 @@
 # script to make the tmux layout
 # doesn't really work right now though...
 
-tmux new-session -s lay1
+# tmux new-session -s lay1
 tmux splitw -t 0 -h -p 35
-tmux splitw -t 0 -v -p 70
-tmux splitw -t 0 -v -p 50
+tmux splitw -v -p 70
+tmux splitw -v -p 50
 tmux selectp -t 0
 tmux splitw -t 0 -v -p 35
